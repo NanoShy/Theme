73347b9d4fe32845ef1905abb42699d0fcf2b3c3
NanoShy <inanotech23@live.com>
2013-08-25 04:53:46 -0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,17 +1,5 @@
-function addGlobalStyle(css){
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if(!head){
-        return;
-    }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+API.on(API.CHAT_COMMAND, callback);
+function callback(value)
+{
+alert(value + ' typed as chat command');
 }
-//changes the background
-addGlobalStyle('body, body {background: #000000 url(localStorage.getItem("http://fc09.deviantart.net/fs71/f/2012/126/a/b/dj_pon3___wallpaper_by_jave_the_13-d4yr3ln.png")) no-repeat;}');
-addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
-
-        RoomUser.audience.roomElements = [];
-    RoomUser.redraw();
