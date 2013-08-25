3732725b09d7ec863fa74a197c9e10bae502199f
NanoShy <inanotech23@live.com>
2013-08-25 04:32:59 -0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -10,7 +10,7 @@ function addGlobalStyle(css){
     head.appendChild(style);
 }
 //changes the background
-addGlobalStyle('body, body {background: #000000 url(localStorage.getItem("backgroundURL")) no-repeat;}');
+addGlobalStyle('body, body {background: #000000 url(localStorage.getItem("http://fc09.deviantart.net/fs71/f/2012/126/a/b/dj_pon3___wallpaper_by_jave_the_13-d4yr3ln.png")) no-repeat;}');
 addGlobalStyle('#room-wheel, #room-wheel {height: 0;}');
 
         RoomUser.audience.roomElements = [];
